bbaf1d0e3e2908959b27439404f6d6e5b24f6f48
s-vivien <simon.vivien.1@gmail.com>
2023-02-04 20:41:34 +0100

    fix: react render upgrade

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import ReactDOM from "react-dom"
+import { createRoot } from 'react-dom/client';
 import "./index.scss"
 import App from "./App"
 import { BrowserRouter } from "react-router-dom";
@@ -7,11 +7,12 @@ import { BrowserRouter } from "react-router-dom";
 // https://caniuse.com/mdn-javascript_builtins_array_flatmap
 require('array.prototype.flatmap').shim()
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root')!);
+
+root.render(
   <React.StrictMode>
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
