375a7355b2c036f62dcac60437919688ec78e752
s-vivien <simon.vivien.1@gmail.com>
2023-02-24 19:07:16 +0100

    feat: display accepted values in playlist edition

--- src/components/Playlist.tsx
+++ src/components/Playlist.tsx
@@ -1,48 +1,22 @@
-import { useContext, useState } from 'react';
+import { useContext } from 'react';
 import { BlindTestContext } from "App";
 import PlaylistSelection from "./PlaylistSelection";
 import PlaylistEdition from './PlaylistEdition';
 import { deleteStoredBlindTestTracks } from 'helpers';
-import { Button, Modal } from 'react-bootstrap';
 
 const Playlist = () => {
 
   const { tracksLoaded, setTracksLoaded } = useContext(BlindTestContext);
 
-  const [confirmationDisplayed, setConfirmationDisplayed] = useState(false);
-
   const restart = () => {
     deleteStoredBlindTestTracks();
-    setConfirmationDisplayed(false);
     setTracksLoaded(false);
   }
 
   return (
     <div>
-      {tracksLoaded && <div className="playlist-load-button mb-2">
-        <Button id="selectList" type="submit" size="sm" onClick={() => setConfirmationDisplayed(true)} title="Select playlist">
-          <b>Load another playlist from Spotify</b>
-        </Button>
-      </div>}
-
-      <Modal show={confirmationDisplayed} centered>
-        <Modal.Body>
-          Do you really want to load another playlist from Spotify ?
-          <br></br>
-          <i>All modifications will be lost</i> (scores won't be affected)
-        </Modal.Body>
-        <Modal.Footer>
-          <Button size="sm" className="mr-2" onClick={restart}>
-            Yes
-          </Button>
-          <Button size="sm" onClick={() => setConfirmationDisplayed(false)}>
-            Cancel
-          </Button>
-        </Modal.Footer>
-      </Modal>
-
       <div id="playlists">
-        {tracksLoaded && <PlaylistEdition />}
+        {tracksLoaded && <PlaylistEdition onRestart={restart} />}
         {!tracksLoaded && <PlaylistSelection />}
       </div>
     </div>
--- src/components/PlaylistEdition.tsx
+++ src/components/PlaylistEdition.tsx
@@ -1,6 +1,6 @@
 import { useContext, useEffect, useState } from 'react'
 import { BlindTestContext } from "App"
-import { cleanValue, deepCopyObject, getStoredBlindTestTracks, setStoredBlindTestTracks } from 'helpers';
+import { cleanValue, deepCopyObject, deleteStoredBlindTestTracks, getStoredBlindTestTracks, setStoredBlindTestTracks } from 'helpers';
 import { Button, Col, Form, Modal, OverlayTrigger, Popover, Row } from 'react-bootstrap';
 import { BlindTestTracks, computeGuessable, Guessable, GuessableType } from './data/BlindTestData';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -11,16 +11,18 @@ type EditedGuessable = {
   disabled: boolean
 }
 
-const PlaylistEdition = () => {
+const PlaylistEdition = (props: any) => {
 
   const { setSubtitle } = useContext(BlindTestContext);
 
   const [bt, setBt] = useState(() => getStoredBlindTestTracks());
   const [selectedIndex, setSelectedIndex] = useState(-1);
+  const [showAcceptedValues, setShowAcceptedValues] = useState(false);
   const [editedValues, setEditedValues] = useState<EditedGuessable[]>([]);
   const [edition, setEdition] = useState(false);
   const [validated, setValidated] = useState(false);
-  const [confirmationDisplayed, setConfirmationDisplayed] = useState(false);
+  const [removeTrackModal, setRemoveTrackModal] = useState(false);
+  const [restartModal, setRestartModal] = useState(false);
 
   useEffect(() => {
     setSubtitle(`Editing playlist`);
@@ -37,6 +39,11 @@ const PlaylistEdition = () => {
       endEdit();
     }
     setValidated(true);
+  }
+
+  const restart = () => {
+    setRestartModal(false);
+    props.onRestart();
   }
 
   const startEdit = (index: number) => {
@@ -79,7 +86,7 @@ const PlaylistEdition = () => {
   const remove = (index: number, confirmed: boolean) => {
     if (!confirmed) {
       setSelectedIndex(index);
-      setConfirmationDisplayed(true);
+      setRemoveTrackModal(true);
     } else {
       let newBt = deepCopyObject(BlindTestTracks, bt);
       newBt.tracks.splice(index, 1);
@@ -88,13 +95,16 @@ const PlaylistEdition = () => {
       if (index < newBt.doneTracks) {
         newBt.doneTracks--;
       }
-      setConfirmationDisplayed(false);
+      setRemoveTrackModal(false);
     }
   }
 
   const renderGuessables = (guessables: Guessable[]) => {
     if (guessables.length === 0) return <></>
-    return guessables.map<React.ReactNode>(g => g.disabled ? <del>{g.original}</del> : <>{g.original}</>).reduce((prev, curr) => [prev, ', ', curr]);
+    return guessables.map<React.ReactNode>(g => {
+      const value = showAcceptedValues ? g.toGuess : g.original;
+      return g.disabled ? <del>{value}</del> : <>{value}</>;
+    }).reduce((prev, curr) => [prev, ', ', curr]);
   }
 
   const popover = (
@@ -117,7 +127,23 @@ const PlaylistEdition = () => {
   return (
     <>
 
-      <Modal show={confirmationDisplayed} centered>
+      <Modal show={restartModal} centered>
+        <Modal.Body>
+          Do you really want to load another playlist from Spotify ?
+          <br></br>
+          <i>All modifications will be lost</i> (scores won't be affected)
+        </Modal.Body>
+        <Modal.Footer>
+          <Button size="sm" className="mr-2" onClick={restart}>
+            Yes
+          </Button>
+          <Button size="sm" onClick={() => setRestartModal(false)}>
+            Cancel
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+      <Modal show={removeTrackModal} centered>
         <Modal.Body>
           Do you really want to remove this track ?
         </Modal.Body>
@@ -125,7 +151,7 @@ const PlaylistEdition = () => {
           <Button size="sm" className="mr-2" onClick={() => remove(selectedIndex, true)}>
             Yes
           </Button>
-          <Button size="sm" onClick={() => setConfirmationDisplayed(false)}>
+          <Button size="sm" onClick={() => setRemoveTrackModal(false)}>
             Cancel
           </Button>
         </Modal.Footer>
@@ -158,7 +184,7 @@ const PlaylistEdition = () => {
                   <Form.Control title={cleanValue(guessable.value)} size="sm" required disabled={guessable.disabled} value={guessable.value} onChange={(e) => { updateValue(index, e.target.value) }} type="text" placeholder="Enter value" />
                 </Col>
                 <Col sm={5}>
-                  <Form.Label size="sm" className="edition-form-cleaned-value">
+                  <Form.Label size="sm" className="code">
                     {cleanValue(guessable.value)}
                   </Form.Label>
                 </Col>
@@ -190,6 +216,21 @@ const PlaylistEdition = () => {
         </Form>
       </Modal>
 
+      <div className="playlist-load-button mb-2">
+        <Button id="selectList" type="submit" size="sm" onClick={() => setRestartModal(true)} title="Select playlist">
+          <b>Load another playlist from Spotify</b>
+        </Button>
+        <Form.Switch
+          type="switch"
+          id="show-accepted"
+          label="Display the values the bot will accept (i.e. cleaned values)"
+          checked={showAcceptedValues}
+          className="m-2"
+          onChange={() => setShowAcceptedValues(!showAcceptedValues)}
+        >
+        </Form.Switch>
+      </div>
+
       {
         bt.tracks.map((track, index) => {
           return <div className={"p-1 edition-row " + (index < bt.doneTracks ? "edition-row-disabled" : "")} key={"track-" + track.offset}>
@@ -199,14 +240,14 @@ const PlaylistEdition = () => {
             <div id="cover" className="edition-cover">
               <img id="cover-image" src={track.img} alt="cover" />
             </div>
-            <div style={{ flex: 1 }}>
-              <div className="px-3">
+            <div style={{ flex: 1 }} className={"px-3 " + (showAcceptedValues ? "code": "")}>
+              <div>
                 <b>{renderGuessables(track.getGuessables(GuessableType.Title))}</b>
               </div>
-              <div className="px-3">
+              <div>
                 {renderGuessables(track.getGuessables(GuessableType.Artist))}
               </div>
-              <div className="px-3">
+              <div>
                 {renderGuessables(track.getGuessables(GuessableType.Misc))}
               </div>
             </div>
@@ -222,3 +263,4 @@ const PlaylistEdition = () => {
 }
 
 export default PlaylistEdition
+
